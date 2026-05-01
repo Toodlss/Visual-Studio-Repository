5eaf436d635fac76b0e2407a1ca39ba7beb0a1e5
Kaden M Bilyeu <kadenbilyeu@yahoo.com>
2026-04-30 23:19:27 -0700

    vs

---
+++ 3VISUAL_STUDIO_HTML_SUBMISSION_ASSIGNMENT/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const box = document.getElementById("animation-box");
+let position = -400;
+
+function animateBox() {
+    position += 2;
+    box.style.left = position + "px";
+
+    if (position >= 400) {
+        position = -400;
+    }
+
+    requestAnimationFrame(animateBox);
+}
+
+animateBox() ;
--- VISUAL STUDIO HTML SUBMISSION ASSIGNMENT/wwwroot/JAVA.js
+++
@@ -1,9 +0,0 @@
-﻿let box = document.getElementById("box");
-let x = 0;
-
-setInterval(() => {
-    x += 5;
-    box.style.left = x + "px";
-
-    if (x > 1800) x = 0;
-}, 30);
